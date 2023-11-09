fa419ee53ac461d1a67706dfbd69477f696108cb
dianacoups <98326872+dianacoups@users.noreply.github.com>
2023-11-10 00:45:28 +0600

    first commit

---
+++ src/Home.js
@@ -0,0 +1,81 @@
+import React, {useState, useEffect} from 'react';
+import {ethers} from 'ethers';
+import constants from "./constants";
+
+function Home() {
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [contractInstance, setContractInstance] = useState('');
+    const [status, setStatus] = useState(false);
+    const [isWinner, setIsWinner] = useState('');
+
+    useEffect(() => {
+        const loadBlockchainData = async () => {
+            if(typeof window.ethereum !== 'undefined') {
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                try{
+                    const signer = provider.getSigner();
+                    const address = await signer.getAddress();
+                    console.log(address);
+                    setCurrentAccount(address);
+                    window.ethereum.on('accountsChanged', (accounts) => {
+                        setCurrentAccount(accounts[0]);
+                        console.log(currentAccount);
+                    })
+                }
+                catch (err) {
+                    console.error(err);
+                }
+            }
+            else {
+                alert('Please install metamask')
+            }
+        };
+
+        const contract = async() => {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contractIns = new ethers.Contract(constants.contractAddress, constants.contractAbi, signer);
+            setContractInstance(contractIns);
+            const status = await contractInstance.isComplete();
+            setStatus(status);
+            const winner = await contractInstance.getWinner();
+            if (winner === currentAccount) {
+                setIsWinner(true);
+            }
+            else {
+                setIsWinner(false);
+            }
+ 
+        }
+
+        loadBlockchainData();
+        contract();
+    }, [currentAccount]);
+
+    const enterLottery  = async () => {
+        const amountToSend = ethers.utils.parseEther('0.001');
+        const tx = await contractInstance.enter({value: amountToSend, });
+        await tx.wait();
+
+    }
+    const claimPrize  = async () => {
+        const tx = await contractInstance.claimPrize();
+        await tx.wait();
+
+    }
+
+
+    return (
+        <div className='container'>
+            <h1>Lottery page</h1>
+            <div className='button-container'>
+                {status ? (isWinner ? (<button className="enter-button" onClick={claimPrize}>Claim prize </button>) : (<p>You are not the winner</p>) ) : (<button className="enter-button" onClick={enterLottery}>Enter Lottery </button>)
+
+                }
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
